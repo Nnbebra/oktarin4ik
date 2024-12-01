e87dec379c56c390b45f19b0add4e141e4446fc9
Nnbebra <maksimdrobysh@gmail.com>
2024-12-01 11:52:53 +0100

    lesson 7

--- main.js
+++ main.js
@@ -35,3 +35,25 @@ window.onscroll = function() {
    activeBtn.classList.remove('active')
  }
 };
+
+document.querySelector(".mobile-button").addEventListener("click", function(event)
+{
+  document.querySelector("mobile-menu").classList.toggle("hide");
+});
+
+menu.addEventListener('click', function(event){
+  let clickTarget = event.target;
+  let activeBtn = document.querySelector('.active');
+  if (clickTarget.classList.contains('nav-link') ){
+      clickTarget.classList.add('active');
+      activeBtn.classList.remove('active');
+
+      let mobile = document.querySelector('.mobile-menu');
+      if (!mobile.classList.contains('hide')){
+          mobile.classList.add('hide');
+      }
+      let menu = document.querySelector(".fixed-menu");
+      menu.addEventListener("click")
+
+  }
+});
